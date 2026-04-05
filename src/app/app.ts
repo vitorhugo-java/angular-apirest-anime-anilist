47abdb405c1d108861bcc2ccc29ad593f0f7c493
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-04-05 13:30:41 -0300

    Refactor: Migrate App component to display seasonal anime with signals and pagination

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,51 @@
-import { Component, signal, inject, OnInit, Signal } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+import { Component, signal, inject, OnInit, Signal, ChangeDetectionStrategy } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Observable } from 'rxjs';
-import { JikanAPI, Anime } from './services/data/jikan-api';
+import { JikanAPI, Anime, Pagination } from './services/data/jikan-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [AsyncPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App implements OnInit {
-  title: Signal<string> = signal('Catálogo de Animes Jikan API');
+  title: Signal<string> = signal('Seasonal Anime');
   private titleService: Title = inject(Title);
   private jikanAPI = inject(JikanAPI);
-  animes$: Observable<Anime[]> = new Observable();
+  animes = signal<Pagination<Anime[]> | null>(null);
 
   ngOnInit(): void {
     this.titleService.setTitle(this.title());
-    this.animes$ = this.jikanAPI.getRecommendations();
-
-    this.animes$.subscribe((animes) => {
-      console.log('Animes recomendados:', animes);
+    this.jikanAPI.getSeasonalAnime().subscribe((data) => {
+      this.animes.set(data);
     });
+  }
+
+  loadMore() {
+    const currentAnimes = this.animes();
+    if (!currentAnimes) return;
+
+    const actualPage: number = currentAnimes.current_page;
+    const nextPage = actualPage + 1;
+
+    this.jikanAPI
+      .getSeasonalAnime(undefined, nextPage)
+      .subscribe((newAnimes: Pagination<Anime[]>) => {
+        this.animes.update((prev) => {
+          if (!prev) return newAnimes;
+          return {
+            ...prev,
+            animes: [...prev.animes, ...newAnimes.animes],
+            current_page: newAnimes.current_page,
+            has_next_page: newAnimes.has_next_page,
+          };
+        });
+      });
+  }
+
+  ellipsis(value: string, limit: number = 100): string {
+    if (value.length <= limit) {
+      return value;
+    }
+    return value.substring(0, limit) + '...';
   }
 }
